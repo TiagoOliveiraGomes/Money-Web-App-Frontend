7f012f23f6dd917875713a6b7c4cee9ddc9f332d
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-09-26 18:31:58 -0300

    add new configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 module.exports = {
-    entry: './src/index.tsx',
+    entry: './src/index.jsx',
     output: {
         path: __dirname + '/public',
-        filename: './app.ts'
+        fileName: './app.js'
     },
     devServer: {
         port: 8080,
-        contentBase: './public',
+        constentBase: './public',
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
-            modules: __dirname + '/node_modules',
-            jquery: 'modules/admin-lte/plugins/jquery/jquery.min.js',
-            bootstrap: 'modules/admin-lte/plugins/bootstrap/js/bootstrap.js'
+            module: __dirname + './node_modules',
+            jquery: 'modules/admin-lte/plugin/jQuery/jquery-2.2.3.min.js',
+            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    plugins: [
+    plugin: [
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
@@ -27,32 +28,20 @@ module.exports = {
         new ExtractTextPlugin('app.css')
     ],
     module: {
-        // loaders: [{
-        //     test: /.ts[x]?$/,
-        //     loader: 'babel-loader',
-        //     exclude: /node_modules/,
-        //     query: {
-        //         presets: ['es2015', 'react'],
-        //         plugins: ['transform-object-rest-spread']
-        //     }, {
-        //         test: /\.css$/,
-        //         loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-        //     }, {
-        //         test: /\.woff|.woff2|.tff|.eot|.svg|.png|.jpg*.*$/,
-        //     },
-        // }]
-        rules:[
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader:'babel-loader'
-                    },
-                ],
-                
-            },
-            
-        ]
+        loaders: [{
+            test: '/.js[x]?$/',
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+            loader: 'file'
+        }]
     }
 }
