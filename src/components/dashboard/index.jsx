46e59679b04f5803f8195f0037cb7bfba12d5326
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-09-29 16:54:47 -0300

    fix

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -8,47 +8,33 @@ import { Content } from '../../common/template/content'
 import { ContentHeader } from '../../common/template/contentHeader'
 import { ValueBox } from '../widget/valueBox'
 
+
 class Dashboard extends Component {
-    componentDidMount(){
+
+    componentWillMount() {
         this.props.getSummary()
     }
+
     render() {
-        const { credit, debt} = this.props.summary
+        const { credit, debt } = this.props.summary
         return (
-            <div>
+            <div> 
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
-                    <Row>
-                        <ValueBox
-                        cols='12 4'
-                        color='green'
-                        icon='bank'
-                        value={`R$ ${credit}`}
-                        text='Total de Créditos'
-                        />
-
-                        <ValueBox
-                        cols='12 4'
-                        color='red'
-                        icon='credit-card'
-                        value={`R$ ${debt}`}
-                        text='Total de Débitos'
-                        />
-
-                        <ValueBox
-                        cols='12 4'
-                        color='blue'
-                        icon='money'
-                        value={`R$ ${credit - debt}`}
-                        text='Valor Consolidado'
-                        />
-                    </Row>
-                </Content>
+                    <Row> 
+                        <ValueBox cols='12 4' color='green' icon='bank'
+                            value={`R$ ${credit}`} text='Total de Créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card'
+                            value={`R$ ${debt}`} text='Total de Débitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money'
+                            value={`R$ ${credit - debt}`} text='Valor Consolidado' />
+                    </Row> 
+                </Content> 
             </div>
         )
     }
 }
 
-const mapStateToProps = state => ({ summary: state.dashboard.summary });
+const mapStateToProps = state => ({summary: state.dashboard.summary})
 const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
--- src/components/dashboard/reducer.js
+++ src/components/dashboard/reducer.js
@@ -1,10 +1,11 @@
 const INITIAL_STATE = {summary: {credit: 0, debt: 0}}
 
 export function DashboardReducer(state = INITIAL_STATE, action) {
-    switch(action.type){
+    switch (action.type) {
         case 'BILLING_SUMMARY_FETCHED':
             return { ...state, summary: action.payload.data }
         default:
             return state
     }
 }
+
