b964207e2c1cee8ba5501f6bd58206d5750a6b36
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-10-03 16:46:53 -0300

    add devtools redux

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,11 +3,12 @@ import { Router, Route, Redirect, hashHistory} from 'react-router'
 
 import { BillingCycle } from '../components/billingCycle'
 import  Dashboard  from '../components/dashboard'
+import  Dashboard2  from '../components/dashboard2'
 
 export function Routes() {
   return (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard} />
+        <Route path='/' component={Dashboard2} />
         <Route path='/billingCycles' component={BillingCycle} />
         <Redirect from='*' to='/' />
     </Router>
