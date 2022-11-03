3a7f075cd18b1eeae030dfd461143424e9c4696e
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-11-02 21:38:32 -0300

    New files

--- src/common/operator/if/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function If(props) {
-    if(props.test){
-        return props.children
-    }else {
-        return false
-    }
-}
--- src/common/tab/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function Tabs(props) {
-  return (
-    <div className='nav-tabs-custom'>
-        {props.children}
-    </div>
-  )
-}
