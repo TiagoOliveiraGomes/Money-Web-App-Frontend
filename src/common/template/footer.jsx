6ec3f00456ecaa7c9f171f984ee8bb33cb069054
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-11-03 14:50:26 -0300

    change

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2017
+            <a href='https://portfolio-tiago-gomes.vercel.app' target='_blank'> Tiago Gomes</a>.
+        </strong>
+    </footer>
+)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,15 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
+<<<<<<< Updated upstream:src/reducers/index.js
 import  DashboardReducer from '../components/dashboard/reducer'
 import  TabReducer from '../common/tab/tabReducer'
 import  BillingCycleReducer  from '../components/billingCycle/reducer'
+=======
+import DashboardReducer from '../dashboard/dashboardReducer'
+import TabReducer from '../common/tab/reducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
+>>>>>>> Stashed changes:src/main/reducers.js
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
